8e79844f278e4856c705316972331283d1fe6e80
hujuu <1961946+hujuu@users.noreply.github.com>
2023-09-30 08:06:27 +0900

    Edit tailwind settings

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,9 @@ import HelloWorld from './components/HelloWorld.vue'
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
 </template>
 
 <style scoped>
